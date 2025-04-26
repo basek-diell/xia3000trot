fcddaa9e7f6fa4f8e6d275fcbed1d15d8ee55c65
basek-diell <basek-diell@ya.ru>
2025-04-26 22:16:08 +0300

    Update configure_zaprets.sh

--- configure_zaprets.sh
+++ configure_zaprets.sh
@@ -14,7 +14,8 @@ find_dnsmasq_section() {
   uci show dhcp | grep "=dnsmasq" | head -n1 | cut -d. -f2 | cut -d= -f1
 }
 
-checkAndAddDomainPermanentName() {
+checkAndAddDomainPermanentName() 
+{
   nameRule="option name '$1'"
   str=$(grep -i "$nameRule" /etc/config/dhcp)
   if [ -z "$str" ]; then
@@ -30,20 +31,32 @@ manage_package() {
   local autostart="$2"
   local process="$3"
 
-  if opkg list-installed | grep -q "^$name"; then
-    if /etc/init.d/$name enabled; then
-      [ "$autostart" = "disable" ] && /etc/init.d/$name disable
-    else
-      [ "$autostart" = "enable" ] && /etc/init.d/$name enable
-    fi
+    if opkg list-installed | grep -q "^$name"; then
+        
+        # Проверка, включен ли автозапуск
+        if /etc/init.d/$name enabled; then
+            if [ "$autostart" = "disable" ]; then
+                /etc/init.d/$name disable
+            fi
+        else
+            if [ "$autostart" = "enable" ]; then
+                /etc/init.d/$name enable
+            fi
+        fi
 
-    if pidof $name > /dev/null; then
-      [ "$process" = "stop" ] && /etc/init.d/$name stop
-    else
-      [ "$process" = "start" ] && /etc/init.d/$name start
+        # Проверка, запущен ли процесс
+        if pidof $name > /dev/null; then
+            if [ "$process" = "stop" ]; then
+                /etc/init.d/$name stop
+            fi
+        else
+            if [ "$process" = "start" ]; then
+                /etc/init.d/$name start
+            fi
+        fi
     fi
-  fi
 }
+
 
 install_youtubeunblock_packages() {
   PKGARCH=$(opkg print-architecture | awk 'BEGIN {max=0} {if ($3 > max) {max = $3; arch = $2}} END {print arch}')
